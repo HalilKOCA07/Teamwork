d5616c854842964dd6c25cd7ac12fb0c563829f3
Sinyour <sinyour52@gmail.com>
2024-01-21 12:44:05 +0300

    ass1-inovation

--- Java-Script/task/002-calculator/calculator2.js
+++ Java-Script/task/002-calculator/calculator2.js
@@ -1,23 +1,24 @@
 let num1 = +(prompt("Enter First Number:"))
 let opr = (prompt("Enter Operator ('+' '-' '*' '/' '%'):"))
 let num2 = +(prompt("Enter Second Number:"))
-10
+
 if (opr == "+"){
-    console.log(num1 + num2)
+    alert(num1 + num2)
 }
 else if (opr == "-"){
-    console.log(num1 - num2);
+    alert(num1 - num2);
 }
 else if (opr == "*"){
-    console.log(num1 * num2);
+    alert(num1 * num2);
 }
 else if (opr == "/"){
-    console.log(num1 / num2);
+    alert(num1 / num2);
 }
 else if (opr == "%"){
-    console.log((num1 / 100) * num2);
+    alert((num1 / 100) * num2);
 }
 else{
-    console.log("Entered type is invlid");
+    alert("Entered type is invlid");
 }
+
 
