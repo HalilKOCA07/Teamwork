fce761329868331d5aa69c9c136e2f228eef1070
Sinyour <sinyour52@gmail.com>
2024-02-25 01:30:01 +0300

    GUESS GAME

--- Java-Script/task/004-guess-game/guess.js
+++ Java-Script/task/004-guess-game/guess.js
@@ -2,71 +2,124 @@
 //*                  EVENTS
 //* ===============================================
 
-const h3 = document.querySelector("h3")
-const addButton = document.getElementById("btn")
-const guessNumber = document.querySelector("#guessingnumber")
-const guessNote = document.querySelector("#guessnote")
-const health = document.querySelector("#remaingright")
-const body = document.querySelector("body")
-const userInput = document.getElementById("userinput")
-let pcSayi = [Math.floor(Math.random() * 101)];
-console.log(pcSayi)
+//* --------------------------------------------
+//?*!                 TANIMLAMA
+//* --------------------------------------------
+const container = document.querySelector(".container"); //=== CONTAINER
+const h3 = document.querySelector("h3"); //=== HEADER
+const addButton = document.getElementById("btn"); //=== BUTTON
+const delButton = document.querySelector(".del-btn"); // === DELETE BUTTON
+const guessNumber = document.querySelector("#guessingnumber"); // === Text
+const guessNote = document.querySelector("#guessnote"); // === BİLGİLENDİRME
+const health = document.querySelector("#remaingright"); // === KALAN HAK
+const body = document.querySelector("body");
+const userInput = document.getElementById("userinput"); // === İNPUT
+let pcSayi = [Math.floor(Math.random() * 101)]; // === RANDOM
+const yourNumber = document.getElementById("yournumber"); // === Girilen Sayılar
+
+//* --------------------------------------------
+//?*!                  STYLE
+//* --------------------------------------------
+
+yourNumber.className = "text-red";
+addButton.className = "button";
+h3.className = "head";
+delButton.className = "d-none";
+
+//* --------------------------------------------
+//?*!                  EVENTS
+//* --------------------------------------------
+
+console.log(pcSayi);
 body.onload = function () {
-    userInput.focus()
-}
+  userInput.focus();
+};
 
 addButton.onclick = (e) => {
+  //! ADD BUTTON
 
-    userInput.focus()
-    guessNote.textContent = `Congratulations! You are right... ${pcSayi} `
+  userInput.focus();
+  const spanEnterNumber = document.createElement("span");
+  spanEnterNumber.textContent = `${userInput.value}   `;
+  yourNumber.appendChild(spanEnterNumber);
+  delButton.className = "d-block";
 
-if (!userInput.value.trim()){
-    guessNote.textContent = `Lutfen  bir sayı giriniz`
-    userInput.value = ""
-    return
-}
-else if (isNaN(userInput.value)){
-    guessNote.textContent = `Lutfen geçerli bir icerik giriniz`
-    userInput.value = ""
-    return
-}
-  
-else if (userInput.value === pcSayi){
-    guessNote.textContent = `Congratulations! You are right...`
-    userInput.value = ""
-    return
-}
 
-else if (userInput.value < 0 || userInput.value > 100) {
-   guessNote.textContent = `You must guess between 1 and 100...`
-   userInput.value = ""
-   return
-}
-  
-else if (userInput.value < pcSayi) {
-   guessNote.textContent = `You must guess a bigger number...`
-   userInput.value = ""
-   return
-}
-  
-else if (userInput.value > pcSayi) {
-   guessNote.textContent = `You must guess a lower number...`
-   userInput.value = ""
-   return
-}
-  
-const spanEnterNumber = document.createElement("span")
-    spanEnterNumber.textContent = `${userInput.value}   `
-    guessNumber.appendChild(spanEnterNumber)
-    console.log(spanEnterNumber)
-    userInput.value = ""
-  
-}
+
+  if (!userInput.value.trim()) {
+    guessNote.textContent = `Lutfen  bir sayı giriniz`;
+    userInput.value = "";
+    return;
+  } else if (isNaN(userInput.value)) {
+    guessNote.textContent = `Lutfen geçerli bir icerik giriniz`;
+    userInput.value = "";
+    return;
+  } else if (userInput.value < 0 || userInput.value > 100) {
+    guessNote.textContent = `You must guess between 1 and 100...`;
+    userInput.value = "";
+    return;
+  } else if (userInput.value < pcSayi) {
+    guessNote.textContent = `You must guess a bigger number...`;
+    userInput.value = "";
+    return;
+  } else if (userInput.value > pcSayi) {
+    guessNote.textContent = `You must guess a lower number...`;
+    guessNote.classList.add("fa-house-chimney", "fa-beat-fade", "text-red");
+    userInput.value = "";
+    return;
+  } else if (userInput.value == pcSayi) {
+    guessNote.textContent = ` `;
+
+    //! DOĞRU SAYI BİLİNDİĞİ ZAMAN SİLİNECEK  ELEMENT
+    guessNote.classList.add("d-none");
+    addButton.classList.add("d-none");
+    userInput.classList.add("d-none");
+    delButton.classList.add("d-none");
+    yourNumber.classList.add("d-none");
+
+    //! DOĞRU SAYI BİLİNDİĞİ ZAMAN EKLENECEK ELEMENT
+    const congrats = document.createElement("i"); //? 1- yeni bir li elementi olustur
+    const textCongrats = document.createTextNode("Congrats! You are right..."); //? 2- Eger    text tabanlı bir element ise text dugumunu olustur
+    congrats.appendChild(textCongrats); //? 3- Olusturulan text dugumunu yeni elemente bagla (append)
+    h3.after(congrats);
+    congrats.classList.add("congrats", "fa-solid", "fa-shake");
+
+    //! DOĞRU SAYI BİLİNDİĞİ ZAMAN EKLENECEK BUTON
+
+
+
+    userInput.value = "";
+    return;
+  }
+  console.log(spanEnterNumber);
+  userInput.value = "";
+};
 
 userInput.onkeydown = function (e) {
-    //   console.log(e.code)
-    if (e.code === "B") {
-      addButton.click()
-    }
+  console.log(e.code);
+  if (e.code == "Enter" || e.code == "NumpadEnter") {
+    addButton.click();
   }
-  
+};
+
+delButton.onclick = (e) => {
+  if (yourNumber.children.length > 0) {
+    yourNumber.lastElementChild.remove();
+  } else if (yourNumber.children.length == 0) {
+    delButton.classList.add("d-none")
+     guessNote.textContent = ``;
+  }
+};
+
+console.log(yourNumber.children.length);
+console.log(yourNumber.children);
+
+yourNumber.onkeydown = function (e) {
+  console.log(e.code);
+  if (e.code == "Enter") {
+    delButton.click();
+  }
+};
+
+
+
