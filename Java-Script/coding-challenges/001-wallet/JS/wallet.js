72fbbcd77c23c0277916caf2e12443b01f1d0826
Sinyour <sinyour52@gmail.com>
2024-03-04 00:08:58 +0300

    wallet JS studying

---
+++ Java-Script/coding-challenges/001-wallet/JS/wallet.js
@@ -0,0 +1,35 @@
+//* Variables
+
+//* GİDER
+const date = document.querySelector("#tarih")
+const harMiktar = document.querySelector("#harcama-miktar")
+const harAlan = document.querySelector("#harcama-alani")
+const harcamaBtn = document.querySelector(".har-kydet-btn")
+
+console.log(typeof date)
+console.log(date.value)
+
+//* GELİR
+const gelirMiktar = document.querySelector(".gelir-input")
+const gelirEkleBtn = document.querySelector(".gelir-btn")
+
+
+//* TABLO
+const table = document.querySelector(".table")
+
+//* RESULT
+
+const calGelir = document.querySelector(".cal-gelir")
+const calGider = document.querySelector(".cal-gider")
+const calKalan = document.querySelector(".cal-kalan")
+
+//* DELETE
+const clearBtn = document.querySelector(".clear-btn")
+
+
+//************************************************************ */
+
+// harcamaBtn.addEventListener("click", () => {
+
+//     if (date.releasePointerCapture)
+// })
