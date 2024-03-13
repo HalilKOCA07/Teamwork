733a7db5011a0f8e9023c6499b281c52bedcf740
Sinyour <sinyour52@gmail.com>
2024-03-13 10:16:19 +0300

    shopping studying (node js)

---
+++ Java-Script/clarusway-projects/006-shopping/vite-project/main.js
@@ -0,0 +1,37 @@
+// import './style.css'
+// import { setupCounter } from './counter.js'
+// import { getProducts } from './src/products.js'
+// // import {app} from "./src/products.js"
+
+// // app()
+
+// getProducts()
+
+
+const allProducts = document.getElementById("pills-all")
+const containerProducts = document.getElementById("pills-tabContent")
+
+ export const getProducts = async () => {
+    try{
+        const responsive = await fetch("https://anthonyfs.pythonanywhere.com/api/products/")
+        console.log(responsive);
+
+        //Error Handing
+        if(!responsive.ok){
+            throw new Error(`${responsive.status}`);
+        }
+        const dataProducts = await responsive.json()
+        console.log(dataProducts);
+        //function gelecek
+    }catch (error) {
+        containerProducts.innerHTML = `<img src="https://www.alastyr.com/blog/wp-content/uploads/2020/11/windows-hatasi.jpg" alt="" />`
+    }
+}
+
+const showProducts = (product) => {
+    const defaultImage = "https://www.alastyr.com/blog/wp-content/uploads/2020/11/windows-hatasi.jpg"
+
+}
+
+export const app = () => console.log("hi");
+getProducts()
